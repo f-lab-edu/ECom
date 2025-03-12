be773369fc23a96f0e06fea2ee6c27c8fe7ddecb
724thomas <724thomas@gmail.com>
2025-03-13 07:49:59 +0900

    Cart, ShippingAddress, User Entity

--- core/src/main/java/com/example/core/domain/cart/Cart.java
+++ core/src/main/java/com/example/core/domain/cart/Cart.java
@@ -1,2 +1,35 @@
-package com.example.core.domain.cart;public class Cart {
+package com.example.core.domain.cart;
+
+import com.example.core.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User user;
+
+    public static Cart of(Long userId) {
+        return Cart.builder()
+                   .userId(userId)
+                   .build();
+    }
 }
