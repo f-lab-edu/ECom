eb516ce87f529e7282651ce39f6dac0b821d1b59
724thomas <724thomas@gmail.com>
2025-04-04 19:17:11 +0900

    refactor: 쿠기 적용으로 jwtutil 로직을 수정하였습니다.

--- core/src/main/java/com/example/core/utils/JwtUtil.java
+++ core/src/main/java/com/example/core/utils/JwtUtil.java
@@ -4,6 +4,9 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -30,84 +33,115 @@ public class JwtUtil {
     private String REFRESH_TOKEN_SECRET;
 
 
-    public String createAccessToken(Long userId, String userEmail, List<String> roles) {
+    public String createAccessToken(Long userId, List<String> roles) {
         Date now = new Date();
         Date expireTime = new Date(now.getTime() + ACCEWSS_TOKEN_EXPIRE_COUNT);
+
         return Jwts.builder()
-                .setSubject(userEmail)
-                .setIssuedAt(now)
-                .setExpiration(expireTime)
-                .signWith(getAccessSecretKey(), SignatureAlgorithm.HS256)
+                .setSubject("AccessToken")
                 .claim("userId", userId)
                 .claim("roles", roles)
                 .claim("type", "access")
+                .setIssuedAt(now)
+                .setExpiration(expireTime)
+                .signWith(getSecretKey(ACCESS_TOKEN_SECRET), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String createRefreshToken(Long userId, String userEmail, List<String> roles) {
+    public String createRefreshToken(Long userId, List<String> roles) {
         Date now = new Date();
         Date expireTime = new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_COUNT);
 
         return Jwts.builder()
-                .setSubject(userEmail)
-                .setIssuedAt(now)
-                .setExpiration(expireTime)
-                .signWith(getRefreshSecretKey(), SignatureAlgorithm.HS256)
+                .setSubject("RefreshToken")
                 .claim("userId", userId)
                 .claim("roles", roles)
                 .claim("type", "refresh")
+                .setIssuedAt(now)
+                .setExpiration(expireTime)
+                .signWith(getSecretKey(REFRESH_TOKEN_SECRET), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public boolean validateAccessToken(String accessToken) {
+    public boolean validateAccessToken(String token) {
+        return validate(token, ACCESS_TOKEN_SECRET);
+    }
+
+    public boolean validateRefreshToken(String token) {
+        return validate(token, REFRESH_TOKEN_SECRET);
+    }
+
+    private boolean validate(String token, String secret) {
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(getAccessSecretKey())
+                    .setSigningKey(getSecretKey(secret))
                     .build()
-                    .parseClaimsJws(accessToken);
+                    .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
-            log.error("Invalid Access Token: {}", e.getMessage());
+            log.error("Invalid Token: {}", e.getMessage());
             return false;
         }
     }
 
-    public boolean validateRefreshToken(String refreshToken) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getRefreshSecretKey())
-                    .build()
-                    .parseClaimsJws(refreshToken);
-            return true;
-        } catch (Exception e) {
-            log.error("Invalid Refresh Token: {}", e.getMessage());
-            return false;
+    public Claims extractAccessClaims(String token) {
+        return extractClaims(token, ACCESS_TOKEN_SECRET);
+    }
+
+    public Claims extractRefreshClaims(String token) {
+        return extractClaims(token, REFRESH_TOKEN_SECRET);
+    }
+
+    private Claims extractClaims(String token, String secret) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey(secret))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Token Secret Key: byte[] 기반
+    private SecretKey getSecretKey(String secret) {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extractAccessTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie: request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    return cookie.getValue();
+                }
+            }
         }
+        return null;
     }
 
-    public Claims extractAccessClaims(String accessToken) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getAccessSecretKey())
-                .build()
-                .parseClaimsJws(accessToken)
-                .getBody();
+    public String extractRefreshTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie: request.getCookies()) {
+                if (cookie.getName().equals("refreshToken")) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 
-    public Claims extractRefreshClaims(String refreshToken) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getRefreshSecretKey())
-                .build()
-                .parseClaimsJws(refreshToken)
-                .getBody();
-    }
+    public void setTokenCookies(HttpServletResponse response,
+                                String accessToken,
+                                String refreshToken) {
+        Cookie accessCookie = new Cookie("accessToken", accessToken);
+        accessCookie.setPath("/");
+        accessCookie.setHttpOnly(true);
+        accessCookie.setMaxAge(ACCEWSS_TOKEN_EXPIRE_COUNT / 1000);
 
-    // Access Token Secret Key: byte[] 기반
-    private SecretKey getAccessSecretKey() {
-        return Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes());
-    }
+        Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
+        refreshCookie.setPath("/api/v1/auth/refresh");
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setSecure(true);
+        refreshCookie.setMaxAge(REFRESH_TOKEN_EXPIRE_COUNT / 1000);
 
-    // Refresh Token Secret Key: byte[] 기반
-    private SecretKey getRefreshSecretKey() {
-        return Keys.hmacShaKeyFor(REFRESH_TOKEN_SECRET.getBytes());
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
     }
 }
