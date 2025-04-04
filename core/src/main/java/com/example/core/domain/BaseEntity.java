c425eb18715ea81659fd522745fce7880bdd251f
724thomas <724thomas@gmail.com>
2025-04-04 19:15:46 +0900

    fix: Getter 추가

--- core/src/main/java/com/example/core/domain/BaseEntity.java
+++ core/src/main/java/com/example/core/domain/BaseEntity.java
@@ -3,12 +3,14 @@ package com.example.core.domain;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @EntityListeners(AuditingEntityListener.class)
