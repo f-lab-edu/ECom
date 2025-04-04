e37e3ca2bb26926d6d9fbf9820b5e0d4ad04ac99
724thomas <724thomas@gmail.com>
2025-04-04 19:15:19 +0900

    refactor: JWT를 json으로 보내주지 않기때문에, 회원정보를 응답으로 보내줄 수 있도록 하였습니다.

--- core/src/main/java/com/example/core/model/AuthResponse.java
+++ core/src/main/java/com/example/core/model/AuthResponse.java
@@ -1,16 +1,32 @@
 package com.example.core.model;
 
+import com.example.core.domain.user.User;
+import com.example.core.domain.user.meta.Status;
 import lombok.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @Builder
 @ToString
 public class AuthResponse implements Serializable {
+    private Long userId;
+    private String userEmail;
+    private String userNickname;
+    private Status userStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    private String accessToken;
-
-    private String refreshToken;
+    public static AuthResponse from(User user) {
+        return AuthResponse.builder()
+                .userId(user.getId())
+                .userEmail(user.getEmail())
+                .userNickname(user.getNickname())
+                .userStatus(user.getStatus())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getModifiedAt())
+                .build();
+    }
 }
