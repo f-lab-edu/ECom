4209b8b672b9faec65cd2db0e7ae237688198bd8
724thomas <724thomas@gmail.com>
2025-04-18 19:37:31 +0900

    fix: Test Profile 설정

--- admin/src/test/java/com/example/admin/AdminApplicationTests.java
+++ admin/src/test/java/com/example/admin/AdminApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("admin-test")
 class AdminApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
--- api/src/test/java/com/example/api/ApiApplicationTests.java
+++ api/src/test/java/com/example/api/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("api-test")
 class ApiApplicationTests {
 
     @Test
