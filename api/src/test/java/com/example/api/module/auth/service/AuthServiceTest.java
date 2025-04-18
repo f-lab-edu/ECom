37bf7e9c3c6df5a1c4257e85941235bbe05ac141
724thomas <724thomas@gmail.com>
2025-04-18 19:40:18 +0900

    fix: Cart가 연결되지 않아서 생긴 test 문제 해결

--- api/src/test/java/com/example/api/module/auth/service/AuthServiceTest.java
+++ api/src/test/java/com/example/api/module/auth/service/AuthServiceTest.java
@@ -2,6 +2,7 @@ package com.example.api.module.auth.service;
 
 import com.example.api.module.auth.controller.request.LoginRequestBody;
 import com.example.api.module.auth.controller.request.SignupRequestBody;
+import com.example.core.domain.cart.api.CartApiRepository;
 import com.example.core.domain.user.User;
 import com.example.core.domain.user.api.UserApiRepository;
 import com.example.core.domain.user.meta.Status;
@@ -18,7 +19,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("api-test")
+@ActiveProfiles({"api-test","core-test"})
 class AuthServiceTest {
 
     @Autowired
@@ -31,11 +32,14 @@ class AuthServiceTest {
     private String nickname = "abc";
     private String password = "Ab1!2345";
     private String phoneNumber = "010-1234-5678";
+    @Autowired
+    private CartApiRepository cartApiRepository;
 
 
     @BeforeEach
     void cleanup() {
         userApiRepository.deleteAll();
+        cartApiRepository.deleteAll();
     }
 
     @Test
@@ -95,7 +99,7 @@ class AuthServiceTest {
         // then
         assertEquals(authResponse.getEmail(), email);
         assertEquals(authResponse.getNickname(), nickname);
-        assertEquals(authResponse.getStatus(), Status.ACTIVE);
+        assertEquals(authResponse.getStatus(), "ACTIVE");
 
         Cookie accessCookie = response.getCookie("accessToken");
         Cookie refreshCookie = response.getCookie("refreshToken");
