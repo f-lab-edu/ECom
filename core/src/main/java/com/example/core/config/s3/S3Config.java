82b7c772b0f66d2100d83ef4584a83fba5411d80
724thomas <724thomas@gmail.com>
2025-05-03 19:13:50 +0900

    feat: 실제 AWS에서 사용될 코드 주석으로 남겨뒀습니다.

--- core/src/main/java/com/example/core/config/s3/S3Config.java
+++ core/src/main/java/com/example/core/config/s3/S3Config.java
@@ -2,6 +2,7 @@ package com.example.core.config.s3;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -26,7 +27,8 @@ public class S3Config {
         return AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(endpoint, "us-east-1"))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+//                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance()) // 실제 AWS에서는 DefaultAWSCredentialsProviderChain을 사용하여 IAM Role을 통해 인증을 받는다.
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))) // Minio 로컬 테스트용.
                 .withPathStyleAccessEnabled(true) // MinIO는 AWS S3와 다르게, 경로 기반으로 요청을 받기 때문에 꼭 켜야 하는 설정
                 .build();
     }
