f2937c506b9b5f712c44377ecd0ab0ffd47bb83e
724thomas <724thomas@gmail.com>
2025-06-27 20:32:19 +0900

    feat: 쿠폰 domain

---
+++ core/src/main/java/com/example/core/domain/coupon/api/CouponApiRepository.java
@@ -0,0 +1,16 @@
+package com.example.core.domain.coupon.api;
+
+import com.example.core.domain.coupon.Coupon;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CouponApiRepository extends JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id = :id")
+    Optional<Coupon> findByIdWithPessimisticLock(Long id);
+}
