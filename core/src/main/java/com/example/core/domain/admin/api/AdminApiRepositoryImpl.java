692ad9027d999be67e4d9d9897ee226104853ed1
724thomas <724thomas@gmail.com>
2025-08-08 03:38:56 +0900

    Update domain models for RBAC relationships

--- core/src/main/java/com/example/core/domain/admin/api/AdminApiRepositoryImpl.java
+++ core/src/main/java/com/example/core/domain/admin/api/AdminApiRepositoryImpl.java
@@ -1,15 +1,15 @@
 package com.example.core.domain.admin.api;
 
-import com.example.core.domain.admin_to_role.QAdminRole;
-import com.example.core.domain.product.api.ProductApiRepositoryCustom;
-import com.example.core.domain.role.QRole;
-import com.example.core.domain.role.Role;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.example.core.domain.admin_to_role.QAdminRole;
+import com.example.core.domain.role.QRole;
+import com.example.core.domain.role.Role;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Repository
